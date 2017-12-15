57175da13e4b862387a1034b515097864590e80e
nhoxbypass@gmail.com <nhoxbypass@gmail.com>
2017-12-15 13:40:41 +0700

    Implement splash screen

---
+++ app/src/main/java/com/iceteaviet/englishnow/SplashActivity.java
@@ -0,0 +1,21 @@
+package com.iceteaviet.englishnow;
+
+import android.os.Build;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            Window w = getWindow();
+            w.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+        }
+    }
+}
