e45d1b642911dac2afda57f13356329286345224
nhoxbypass@gmail.com <nhoxbypass@gmail.com>
2017-12-22 09:48:47 +0700

    add login screen

---
+++ app/src/main/java/com/iceteaviet/englishnow/UserTypeChooserActivity.java
@@ -0,0 +1,13 @@
+package com.iceteaviet.englishnow;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class UserTypeChooserActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_type_chooser);
+    }
+}
