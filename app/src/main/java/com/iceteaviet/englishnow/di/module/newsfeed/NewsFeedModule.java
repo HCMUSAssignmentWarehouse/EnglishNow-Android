c1413632644390bc7366221168f8b227ca7575a0
nhoxbypass@gmail.com <nhoxbypass@gmail.com>
2018-01-14 23:22:44 +0700

    Fix bug: IllegalStateException: Recursive entry to executePendingTransactions

--- app/src/main/java/com/iceteaviet/englishnow/di/module/newsfeed/NewsFeedModule.java
+++ app/src/main/java/com/iceteaviet/englishnow/di/module/newsfeed/NewsFeedModule.java
@@ -22,6 +22,6 @@ public class NewsFeedModule {
 
     @Provides
     public NewsFeedPagerAdapter provideNewsFeedPagerAdapter(NewsFeedFragment fragment) {
-        return new NewsFeedPagerAdapter(fragment.getFragmentManager());
+        return new NewsFeedPagerAdapter(fragment.getChildFragmentManager());
     }
 }
